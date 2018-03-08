2bc12ffce3162d1a0636abce1254a411a75adf9e
elisee-nds <elisee.reclus@nds.k12.tr>
2018-03-08 13:57:06 +0300

    test

--- www/js/index.js
+++ www/js/index.js
@@ -57,21 +57,4 @@
 	
 	
 	
- function getAcceleration()
- {
-	  
- }
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', getAcceleration, false);
-    },
-    
-};
+ 
